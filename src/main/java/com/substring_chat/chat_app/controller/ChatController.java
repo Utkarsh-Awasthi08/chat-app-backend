59b78e53b0c4b594e98373168c7c93ecd0ecdbeb
Utkarsh-Awasthi08 <utkarsha070@gmail.com>
2025-09-29 23:08:01 +0530

    Fixed bugs

--- src/main/java/com/substring_chat/chat_app/controller/ChatController.java
+++ src/main/java/com/substring_chat/chat_app/controller/ChatController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.time.LocalDateTime;
 
 @Controller
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin("*")
 public class ChatController {
 
     private RoomService roomService;
